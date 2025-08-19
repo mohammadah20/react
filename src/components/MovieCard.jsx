4b2638f9ac827beb480ab828a80c6ac752d6b1be
mohammadah20 <ahmadi.moghaddam.1384@gmail.com>
2025-08-19 15:57:26 +0330

    update project

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,18 +5,25 @@ import { Link } from "react-router-dom";
 const TMDB_IMAGE_BASE = "https://image.tmdb.org/t/p/"
 
 const buildPosterUrl = (movie, size = "w500") => {
-  const path = movie?.poster_path
-  return path ? `${TMDB_IMAGE_BASE}${size}${path}` : "/placeholder-poster.svg"
+ if (movie?.poster_path) {
+    return `https://image.tmdb.org/t/p/${size}${movie.poster_path}`
+  }
+  
+  if (movie?.poster_url) {
+    return movie.poster_url
+  }
+ 
+  return "/placeholder-poster.svg"
 }
-  const MovieCard = (props ,{movie : { title , vote_average , poster_path , release_date , original_language }}) => {
-
+  const MovieCard = ({movie }) => {
+   const movieId=movie.id || movie.movie_id;
     return(
          <li>
       <Link to={`/movie/${movie.id}`}>
         <img src={buildPosterUrl(movie)} alt={movie.title} loading="lazy" />
       </Link>
       <h3>
-        <Link to={`/movie/${movie.id}`}>{props.movie.title}</Link>
+        <Link to={`/movie/${movie.id}`}>{movie.title}</Link>
       </h3>
     </li>
         
