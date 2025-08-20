fef50ba78cc3a25a09fe82c43326cd8f9b196bec
mohammadah20 <ahmadi.moghaddam.1384@gmail.com>
2025-08-20 09:45:44 +0330

    update project

--- src/App.jsx
+++ src/App.jsx
@@ -88,8 +88,8 @@ const App = () => {
       <div className="wrapper">
         <header>
           <img src="./hero-img.svg" alt="Hero Banner" />
-          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
-
+          <h1>You Will Find <span className="text-gradient">Movies</span> That You Will Enjoy </h1>
+e
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
 
